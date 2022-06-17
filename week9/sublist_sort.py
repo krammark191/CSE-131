0d285502f4d7c085a66a519cb34940939e06e851
Mark Van Horn <krammark191@gmail.com>
2022-06-17 16:27:38 -0600

    Finished the program

--- week9/sublist_sort.py
+++ week9/sublist_sort.py
@@ -11,48 +11,104 @@
 #      the same method as the original list. The sub-lists will then be
 #      merged back together to create a sorted list.
 # 4. What was the hardest part? Be as specific as possible.
-#      -a paragraph or two about how the assignment went for you-
+#      The assignment was actually quite simple, but the hardest part was
+#      figuring out how to sort the sub-lists.
 # 5. How long did it take for you to complete the assignment?
-#      -total time in hours including reading the assignment and submitting the program-
+#      About 1 hour.
+
+import random
+import time
+
+def combine(source, destination, begin1, begin2, end2):
+    end1 = begin2
+    for i in range(begin1, end2):
+        if begin1 < end1 and (begin2 == end2 or source[begin1] < source[begin2]):
+            destination[i] = source[begin1]
+            begin1 += 1
+        else:
+            destination[i] = source[begin2]
+            begin2 += 1
+    return destination
+
+def sort(array):
+    size = len(array)
+    src = array
+    des = [None for i in range(len(array))]
+    num = 2
+
+    while num > 1:
+        num = 0
+        begin1 = 0
+
+        while begin1 < size:
+            end1 = begin1 + 1
+            while end1 < size and src[end1 - 1] <= src[end1]:
+                end1 += 1
+            
+            begin2 = end1
+            if begin2 < size:
+                end2 = begin2 + 1
+            else:
+                end2 = begin2
+            while end2 < size and src[end2 - 1] <= src[end2]:
+                end2 += 1
+            
+            num += 1
+            combine(src, des, begin1, begin2, end2)
+            begin1 = end2
+        temp = src
+        src = des
+        des = temp
+    return src
+
+def handle(array):
+    if len(array) > 0:
+        print(f"Unsorted list: {array}")
+        array = sort(array)
+        print(f"Sorted list: {array}")
+    else:
+        print(f"Unsorted list: {array}")
+        print("The list is empty.")
+
 
 def main():
-    """
-    This function will run the program.
-    """
-    file = get_file() # get the file
-    list = get_list(file) # get the list
-    print(list) # print the list
-    create_sub_lists(list) # create the sub-lists
-    print(list) # print the sorted list
-    file.close() # close the file
-    pass
+    mode = input("Enter 's' for standard mode or 't' for test mode: ")
+    if mode == 's':
+        arr = list(input("Enter a list of numbers separated by spaces: ").split())
+        handle(arr)
+    elif mode == 't':
+        time.sleep(0.5)
+        print("\nTest 1: Empty list")
+        arr = []
+        handle(arr)
+        time.sleep(0.5)
+        print("\nTest 2: List of one number")
+        arr = [1,1,1,1]
+        handle(arr)
+        time.sleep(0.5)
+        print("\nTest 3: Single element list")
+        arr = [1]
+        handle(arr)
+        time.sleep(0.5)
+        print("\nTest 4: Reverse sorted list")
+        arr = [4,3,2,1]
+        handle(arr)
+        time.sleep(0.5)
+        print("\nTest 5: List of strings")
+        arr = ["yes", "no", "maybe", "so"]
+        handle(arr)
+        time.sleep(0.5)
+        print("\nTest 6: List of boolean values")
+        arr = [True, False, True, False]
+        handle(arr)
+        time.sleep(0.5)
+        print("\nTest 7: List of mixed values")
+        arr = [random.randint(0, 1000) for i in range(20)]
+        handle(arr)
+        time.sleep(0.5)
+        print("\nTest 8: List of mixed values")
+        arr = [9,6,3,8,6,4,3,2,66,6,4,3,2,1,0,9,8,7,6,5,4,3,2,1,0]
+        handle(arr)
+        print()
 
-def get_file():
-    """
-    This function will get the file name from the user.
-    Then it will open the file and read the contents.
-    """
-    file_name = input("Enter the file name: ") # get the file name from the user
-    try: # try to open the file
-        file = open(file_name, "r") # open the file
-    except FileNotFoundError: # if the file is not found
-        print("File not found.") # print error message
-        return get_file() # call the function again
-    return file # return the file
-
-def get_list(file):
-    """
-    This function will get the list from the file.
-    """
-    list = [] # create an empty list
-    for line in file: # for each line in the file
-        list.append(int(line)) # append the line to the list
-    return list # return the list
-
-def create_sub_lists(list):
-    """
-    This function will create two sub-lists.
-    """
-    sub_list_1 = 0 # create an empty sub-list index
-    sub_list_2 = 0 # create an empty sub-list index
-    return list
+main()
